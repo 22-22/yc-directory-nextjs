84b14014bbd7a04ce20001e1fc61b24f6083e0ba
Yuliya Kogol <kogol.julia@gmail.com>
2025-05-19 10:31:05 +0200

    feat: add a startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -7,17 +8,44 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
+  const posts = [{
+    _createdAt: "11.11.2022",
+    views: 55,
+    author: { _id: 1, name: "Jane Doe", image: "https://dummyjson.com/image/100" },
+    _id: 1,
+    description: "This is a description",
+    image: "https://dummyjson.com/image/150",
+    category: "Robots",
+    title: "We Robots",
+  }]
+
   return (
-    <section className="w-full bg-primary-default min-h-[530px] flex justify-center items-center flex-col py-10 px-6">
-      <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
-        Pitch Your Startup, <br />
-        Connect With Entrepreneurs
-      </h1>
-      <p className="font-medium text-[20px] text-white max-w-2xl text-center break-words">
-        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-        Competitions.
-      </p>
-      <SearchForm query={query} />
-    </section>
+    <>
+      <section className="w-full bg-primary-default min-h-[530px] flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="font-medium text-[20px] text-white max-w-2xl text-center break-words">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="px-6 py-10 max-w-7xl mx-auto">
+        <p className="font-semibold text-[30px] text-black">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 grid md:grid-cols-3 sm:grid-cols-2 gap-5">
+          {posts?.length > 0 ? (
+            posts.map((post: any) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
